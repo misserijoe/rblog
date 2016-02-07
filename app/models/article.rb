69152768054e808f6306054f380c642d4494e76b
misserijoe <misserijoe@gmail.com>
2016-02-06 21:12:33 -0800

    Deleting associated objects

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
 	# Ensures that all articles have a title that is at least five characters long.
-	has_many :comments
+	has_many :comments, dependent: :destroy
 	validates :title, presence: true, length: { minimum: 5 }
 end
